24f824f596a5f0b10101807d8fc598ea64f65a9d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-07-25 18:51:15 -0400

    Fix Register validation

--- validation/register.js
+++ validation/register.js
@@ -3,8 +3,42 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  const key_attributes = ['name', 'email', 'password', 'password2'];
+
+  key_attributes.forEach(key => {
+    data[key] = !isEmpty(data[key]) ? data[key] : '';
+  });
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email field does not contain a valid email address';
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Password must match';
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
   }
 
   return {
