24f68bda400e2749372c6ce93c11471a0d7c36ba
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-07-28 23:41:01 -0400

    Add route to delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -306,4 +306,19 @@ router.delete(
   }
 );
 
+// @route DELETE api/profile/
+// @desc  Delete user and profile
+// @access Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(profile => {
+      User.findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(404).json(err));
+    });
+  }
+);
+
 module.exports = router;
