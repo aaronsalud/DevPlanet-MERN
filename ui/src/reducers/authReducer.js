d96149d6d504aa27c242db8cc78b08c8c192287b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-02 21:28:35 -0400

    Add authReducer

---
+++ ui/src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    // case SET_CURRENT_USER:
+
+    //     break;
+
+    default:
+      return state;
+  }
+}
