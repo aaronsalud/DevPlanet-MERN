54705611980b60bd1f433dd2f5f156e3a0cfbf81
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-03 22:17:10 -0400

    Add action creator method for logging in users

--- ui/src/actions/authActions.js
+++ ui/src/actions/authActions.js
@@ -8,3 +8,17 @@ export const registerUser = (userData, history) => dispatch => {
     .then(response => history.push('/login'))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
+
+//Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(response => {
+      // Save to localStorage
+      const { token } = response.data;
+      // Set token to local storage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+    })
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
