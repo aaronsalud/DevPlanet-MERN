90761bc3df06c978ba50f5e90f446d45aad8cd63
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-03 23:06:35 -0400

    Add functionality to check authentication for every page request

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,6 +13,16 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
 import './App.css';
+
+// Check to see if user is already logged in for every page request
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticted
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
