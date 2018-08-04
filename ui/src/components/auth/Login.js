85e1e3bfb7d1d6cd7e8d5454f0e13c816e44c057
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-03 22:58:28 -0400

    Display error messages to login form

--- ui/src/components/auth/Login.js
+++ ui/src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
 
@@ -24,9 +25,20 @@ class Login extends Component {
       email: this.state.email,
       password: this.state.password
     };
-    console.log(loginCredentials);
+    this.props.loginUser(loginCredentials);
   };
+
+  componentWillReceiveProps(nextProps) {
+    // Redirect user if already logged in
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -38,22 +50,32 @@ class Login extends Component {
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
