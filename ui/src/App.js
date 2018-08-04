637d5ea69bb2b1a4da15e1a56752f0ee125d1df8
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-03 23:41:32 -0400

    Check for user token expiry and redirect back to login page

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -22,6 +22,15 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticted
   store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+    // Redirect back to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
