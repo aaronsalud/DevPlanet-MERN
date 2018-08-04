790fd3886cbbf094e22adf580c7b3fdbc5122fbb
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-03 23:15:45 -0400

    Connect Navbar component with the redux store

--- ui/src/components/layout/Navbar.js
+++ ui/src/components/layout/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUSer } from '../../actions/authActions';
 
 class Navbar extends Component {
   render() {
@@ -46,4 +49,15 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUSer }
+)(Navbar);
