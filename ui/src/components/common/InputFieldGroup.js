f327401d07bac264b94804fd62b6dcd70ca4f143
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-06 20:58:31 -0400

    Add InputFieldGroup component

---
+++ ui/src/components/common/InputFieldGroup.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const InputFieldGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i classNAme={icon} />
+        </span>
+      </div>
+      <input
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  placeholder: PropTypes.string,
+  error: PropTypes.string
+};
+
+InputFieldGroup.defaultProps = {
+  type: 'text'
+};
+
+export default InputFieldGroup;
