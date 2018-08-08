da24e52572a33002649ca4f1983f71acdcfd1df1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-08 14:39:22 -0400

    Add createProfile action

--- ui/src/actions/profileActions.js
+++ ui/src/actions/profileActions.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
+} from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -21,6 +26,15 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(response => history.push('/dashboard'))
+    .catch(error =>
+      dispatch({ type: GET_ERRORS, payload: error.response.data })
+    );
+};
 // Profile Loading
 export const setProfileLoading = () => {
   return {
