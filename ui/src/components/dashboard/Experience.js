2aead12e34508c87e1a22463dcaf25e75c3e8594
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-08 18:23:40 -0400

    Add formated date

--- ui/src/components/dashboard/Experience.js
+++ ui/src/components/dashboard/Experience.js
@@ -6,13 +6,18 @@ import Moment from 'react-moment';
 
 class Experience extends Component {
   render() {
+    console.log(this.props.data);
     const experience = this.props.data.map(exp => (
       <tr key={exp._id}>
         <td>{exp.title}</td>
         <td>{exp.company}</td>
         <td>
-          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{' '}
-          <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? (
+            ' Present'
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
         </td>
         <td>
           <button className="btn btn-danger">Delete</button>
