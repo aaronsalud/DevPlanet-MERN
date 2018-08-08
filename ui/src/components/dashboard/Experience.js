7e255ea41fa6c75aeab45e350a940df92bb8f829
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-08 17:36:19 -0400

    Add Experience component

---
+++ ui/src/components/dashboard/Experience.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
+
+class Experience extends Component {
+  render() {
+    const experience = this.props.data.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.title}</td>
+        <td>{exp.company}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{' '}
+          <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
+
+    return (
+      <div>
+        <h4 className="mb-4">Experience Credentials</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Company</th>
+              <th>Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{experience}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+Experience.propTypes = {
+  data: PropTypes.array.isRequired
+};
+
+export default connect(null)(withRouter(Experience));
