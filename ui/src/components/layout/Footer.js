35d97d3dcf5990ea45415ccb5487ab3aad771347
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-09 20:32:09 -0400

    Rename Footer

--- ui/src/components/layout/Footer.js
+++ ui/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div>
       <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; {new Date().getFullYear()} Dev Hub
+        Copyright &copy; {new Date().getFullYear()} Dev Planet
       </footer>
     </div>
   );
