6510268e4e37fd525075c9d28d313360afdb3dd7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-09 20:32:36 -0400

    Fix account type name

--- ui/src/components/auth/Login.js
+++ ui/src/components/auth/Login.js
@@ -51,7 +51,9 @@ class Login extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your DevHub account</p>
+              <p className="lead text-center">
+                Sign in to your Dev Planet account
+              </p>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
                   placeholder="Email Address"
