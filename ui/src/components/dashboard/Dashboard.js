2496b3431c7f60e92341f0b09d2f86865c974375
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-11 19:02:14 -0400

    Fix link

--- ui/src/components/dashboard/Dashboard.js
+++ ui/src/components/dashboard/Dashboard.js
@@ -29,7 +29,8 @@ class Dashboard extends Component {
         dashboardContent = (
           <div>
             <p className="lead text-muted">
-              Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+              Welcome{' '}
+              <Link to={`/profile/${profile.user._id}`}>{user.name}</Link>
             </p>
             <ProfileActions />
             <Experience data={profile.experience} />
