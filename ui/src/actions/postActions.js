31ccb34ec9c7606b9174f9fa926ce01f421db9dc
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-11 15:09:46 -0400

    Add action creator method to fetch all posts

--- ui/src/actions/postActions.js
+++ ui/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -15,4 +15,25 @@ export const addPost = postData => dispatch => {
     .catch(error =>
       dispatch({ type: GET_ERRORS, payload: error.response.data })
     );
+};
+
+// Get All Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostsLoading);
+  axios
+    .get('/api/posts')
+    .then(response =>
+      dispatch({
+        type: GET_POSTS,
+        payload: response.data
+      })
+    )
+    .catch(error => dispatch({ type: GET_POSTS, payload: null }));
+};
+
+// Set loading state
+export const setPostsLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
