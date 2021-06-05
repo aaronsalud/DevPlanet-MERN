c3a87fa1bba924ab12c9f4461169c6da9bf1e01d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-05 19:29:09 -0400

    Update PostItem.js

--- ui/src/components/posts/PostItem.js
+++ ui/src/components/posts/PostItem.js
@@ -14,30 +14,30 @@ const PostItem = ({ post, auth, showActions, deletePost, addLike, removeLike }) 
       <div className="row">
         <div className="col-md-2">
           <Link to={`/profile/${post.user}`}>
-            <img className="rounded-circle d-none d-md-block" src={post.avatar} alt=""/>
+            <img className="rounded-circle d-none d-md-block" src={post.avatar} alt="" />
           </Link>
           <br />
           <p className="text-center">{post.name}</p>
         </div>
         <div className="col-md-10">
           <p className="lead">{post.text}</p>
-          {showActions ? (
+          {showActions && (
             <span>
               <button onClick={() => addLike(post._id)} type="button" className="btn btn-light mr-1">
-                <i className={classnames('fas fa-thumbs-up', { 'text-info': findUserLike(post.likes)})}/>
+                <i className={classnames('fas fa-thumbs-up', { 'text-info': findUserLike(post.likes) })} />
                 <span className="badge badge-light">{post.likes.length}</span>
               </button>
               <button onClick={() => removeLike(post._id)} type="button" className="btn btn-light mr-1">
                 <i className="text-secondary fas fa-thumbs-down" />
               </button>
               <Link to={`/post/${post._id}`} className="btn btn-info mr-1">Comments</Link>
-              {post.user === auth.user.id ? (
+              {post.user === auth.user.id && (
                 <button className="btn btn-danger mr-1" type="button" onClick={() => deletePost(post._id)}>
-                  {' '}<i className="fas fa-times" />{' '}
+                  <i className="fas fa-times" />
                 </button>
-              ) : null}
+              )}
             </span>
-          ) : null}
+          )}
         </div>
       </div>
     </div>
