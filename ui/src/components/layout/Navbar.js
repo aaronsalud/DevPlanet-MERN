1751854871e32e41933bc8903f81bd59e3973063
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-07 18:18:47 -0400

    Rewrite Navbar component

--- ui/src/components/layout/Navbar.js
+++ ui/src/components/layout/Navbar.js
@@ -1,91 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
 import { clearCurrentProfile } from '../../actions/profileActions';
 
-class Navbar extends Component {
-  onLogoutClick = e => {
+const Navbar = ({ auth, clearCurrentProfile, logoutUser }) => {
+  const onLogoutClick = e => {
     e.preventDefault();
-    this.props.clearCurrentProfile();
-    this.props.logoutUser();
+    clearCurrentProfile();
+    logoutUser();
   };
-  render() {
-    const { isAuthenticated, user } = this.props.auth;
 
-    const authLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to="/feed" className="nav-link">
-            Post Feed
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/dashboard" className="nav-link">
-            Dashboard
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a onClick={this.onLogoutClick} href="/" className="nav-link">
-            <img
-              className="rounded-circle"
-              style={{ width: '25px', marginRight: '5px' }}
-              src={user.avatar}
-              alt={user.name}
-              title="You must have a Gravatar connected to your email to display an image "
-            />{' '}
-            Logout
-          </a>
-        </li>
-      </ul>
-    );
+  const { isAuthenticated, user } = auth;
 
-    const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            Login
-          </Link>
-        </li>
-      </ul>
-    );
+  const loadAuthLinks = () => (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link to="/feed" className="nav-link">Post Feed</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/dashboard" className="nav-link">Dashboard</Link>
+      </li>
+      <li className="nav-item">
+        <a onClick={onLogoutClick} href="/" className="nav-link">
+          <img className="rounded-circle" style={{ width: '25px', marginRight: '5px' }} src={user.avatar} alt={user.name} title="You must have a Gravatar connected to your email to display an image " />
+          <span style={{ marginLeft: '2px' }}>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
 
-    return (
-      <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            DevPlanet
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-nav"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+  const loadGuestLinks = () => (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/register">Sign Up</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/login">Login</Link>
+      </li>
+    </ul>
+  );
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/profiles">
-                  Developers
-                </Link>
-              </li>
-            </ul>
-
-            {isAuthenticated ? authLinks : guestLinks}
-          </div>
+  return (
+    <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark mb-4">
+      <div className="container">
+        <Link className="navbar-brand" to="/">DevPlanet</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="mobile-nav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/profiles">Developers</Link>
+            </li>
+          </ul>
+          {isAuthenticated ? loadAuthLinks() : loadGuestLinks()}
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 Navbar.propTypes = {
