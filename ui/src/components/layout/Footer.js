731f46dea26b6884cd34f874155e6537cd117b95
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-07 18:11:42 -0400

    Rewrite Footer component

--- ui/src/components/layout/Footer.js
+++ ui/src/components/layout/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <div>
-      <footer className="bg-dark text-white mt-5 p-4 fixed-bottom text-center">
-        Copyright &copy; {new Date().getFullYear()} Dev Planet
+export default () => (
+  <div>
+    <footer className="bg-dark text-white mt-5 p-4 fixed-bottom text-center">
+      Copyright &copy; {new Date().getFullYear()} Dev Planet
       </footer>
-    </div>
-  );
-};
+  </div>
+);
+
