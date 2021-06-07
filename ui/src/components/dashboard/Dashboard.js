5a16e2ba81782b314b4e6600f7cce0937415df5f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-07 18:29:38 -0400

    Rewrite Dashboard component

--- ui/src/components/dashboard/Dashboard.js
+++ ui/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -8,69 +8,57 @@ import ProfileActions from './ProfileActions';
 import Experience from './Experience';
 import Education from './Education';
 
-class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getCurrentProfile();
-  }
+const Dashboard = ({ auth, profile, getCurrentProfile, deleteAccount }) => {
 
-  onDeactivateClick = e => {
-    this.props.deleteAccount();
-  };
-  render() {
-    const { user } = this.props.auth;
-    const { profile, loading } = this.props.profile;
+  useEffect(() => getCurrentProfile(), []);
 
-    let dashboardContent;
-    if (!profile && loading) {
-      dashboardContent = <Spinner />;
-    }
+  const { user } = auth;
+  const { loading } = profile;
+  const profileData = profile.profile;
+
+  const loadContent = () => {
+    if (!profileData && loading) return <Spinner />
+
     // Check if logged in user has profile data
-    else if (profile && Object.keys(profile).length > 0) {
-      dashboardContent = (
+    else if (profileData && Object.keys(profileData).length > 0) {
+      return (
         <div>
           <p className="lead text-muted">
-            Welcome{' '}
-            <Link to={`/profile/${profile.user._id}`}>{user.name}</Link>
+            <span style={{marginRight:'5px'}}>Welcome</span>
+            <Link to={`/profile/${profileData.user._id}`}>{user.name}</Link>
           </p>
           <ProfileActions />
-          <Experience data={profile.experience} />
-          <Education data={profile.education} />
+          <Experience data={profileData.experience} />
+          <Education data={profileData.education} />
           <div style={{ marginBottom: '60px' }}>
-            <button
-              onClick={this.onDeactivateClick}
-              className="btn btn-danger"
-            >
-              Deactivate My Account
-              </button>
+            <button onClick={e => deleteAccount()} className="btn btn-danger">Deactivate My Account</button>
           </div>
         </div>
       );
-    } else {
-      // User is logged in but has no profile
-      dashboardContent = (
-        <div>
-          <p className="lead text-muted">Welcome {user.name}</p>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-lg btn-info">
-            Create Profile
-            </Link>
-        </div>
-      );
     }
-
+    // User is logged in but has no profile
     return (
-      <div className="dashboard">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h1 className="display-4">Dashboard</h1>
-              {dashboardContent}
-            </div>
+      <div>
+        <p className="lead text-muted">Welcome {user.name}</p>
+        <p>You have not yet setup a profile, please add some info</p>
+        <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+      </div>
+    );
+
+  }
+
+  return (
+    <div className="dashboard">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="display-4">Dashboard</h1>
+            {loadContent()}
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
