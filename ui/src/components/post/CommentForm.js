69b419855f14ff4b4cf75cf630c2ffec073ac497
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-07 18:00:00 -0400

    Rewrite CommentForm component

--- ui/src/components/post/CommentForm.js
+++ ui/src/components/post/CommentForm.js
@@ -1,61 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { addComment } from '../../actions/postActions';
-class CommentForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    };
-  }
 
-  onSubmit = e => {
+const CommentForm = ({ auth, postId, addComment, errors }) => {
+  const [text, setText] = useState('');
+  const onSubmit = e => {
     e.preventDefault();
-    const { user } = this.props.auth;
-    const { postId } = this.props;
+    const { name, avatar } = auth.user;
+    const newComment = { text, name, avatar };
+    addComment(postId, newComment);
+    setText('');
+  };
 
-    const newComment = {
-      text: this.state.text,
-      name: user.name,
-      avatar: user.avatar
-    };
-    this.props.addComment(postId, newComment);
-    this.setState({ text: '' });
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  render() {
-    const { errors } = this.props;
-    return (
-      <div className="post-form mb-3">
-        <div className="card card-info">
-          <div className="card-header bg-info text-white">Add a Comment</div>
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <TextAreaFieldGroup
-                  placeholder="Reply to this post"
-                  name="text"
-                  value={this.state.text}
-                  onChange={this.onChange}
-                  error={errors.text}
-                />
-              </div>
-              <button type="submit" className="btn btn-dark">
-                Submit
-              </button>
-            </form>
-          </div>
+  return (
+    <div className="post-form mb-3">
+      <div className="card card-info">
+        <div className="card-header bg-info text-white">Add a Comment</div>
+        <div className="card-body">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <TextAreaFieldGroup placeholder="Reply to this post" name="text" value={text} onChange={e => setText(e.target.value)} error={errors.text} />
+            </div>
+            <button type="submit" className="btn btn-dark">Submit</button>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 CommentForm.propTypes = {
   addComment: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
