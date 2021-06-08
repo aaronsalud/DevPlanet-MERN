c4c1d9c41766dcad81337d5cfef7f3e3aaaf6953
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-06-08 18:21:25 -0400

    Update Experience.js

--- ui/src/components/dashboard/Experience.js
+++ ui/src/components/dashboard/Experience.js
@@ -1,56 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import { deleteExperience } from '../../actions/profileActions';
 
-class Experience extends Component {
-  onDeleteClick = id => {
-    this.props.deleteExperience(id);
-  };
-  render() {
-    const experience = this.props.data.map(exp => (
-      <tr key={exp._id}>
-        <td>{exp.title}</td>
-        <td>{exp.company}</td>
-        <td>
-          <Moment format="YYYY/MM/DD">{exp.from.split('T')[0]}</Moment> -{' '}
-          {!exp.to ? (
-            ' Present'
-          ) : (
-            <Moment format="YYYY/MM/DD">{exp.to.split('T')[0]}</Moment>
-          )}
-        </td>
-        <td>
-          <button
-            onClick={() => {
-              this.onDeleteClick(exp._id);
-            }}
-            className="btn btn-danger"
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    ));
+const Experience = ({ data, deleteExperience }) => {
+  const loadExperience = () => data.map(exp => (
+    <tr key={exp._id}>
+      <td>{exp.title}</td>
+      <td>{exp.company}</td>
+      <td>
+        <Moment format="YYYY/MM/DD">{exp.from.split('T')[0]}</Moment> -{' '}
+        {!exp.to ? (' Present') : (
+          <Moment format="YYYY/MM/DD">{exp.to.split('T')[0]}</Moment>
+        )}
+      </td>
+      <td>
+        <button onClick={() => deleteExperience(exp._id)} className="btn btn-danger">Delete</button>
+      </td>
+    </tr>
+  ));
 
-    return (
-      <div>
-        <h4 className="mb-4">Experience Credentials</h4>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Company</th>
-              <th>Years</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>{experience}</tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h4 className="mb-4">Experience Credentials</h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Company</th>
+            <th>Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{loadExperience()}</tbody>
+      </table>
+    </div>
+  );
 }
 
 Experience.propTypes = {
